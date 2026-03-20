1cb2e71264857b00f4f7d3c0bcac20ffcf9bb6a9
lihe-source <lihedalihome@gmail.com>
2026-03-20 19:59:11 +0800

    v3.4

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
  *  - 右上角顯示 screener.json 的實際資料日期
  */
 
-const APP_VERSION = 'V3.2';
+const APP_VERSION = 'V3.4';
 
 const CFG = {
   SCREENER_JSON: './data/screener.json',  // 預計算資料
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'taiwan-stock-radar-v3.2';
+const CACHE_NAME = 'taiwan-stock-radar-v3.4';
 const STATIC_ASSETS = [
   './',
   './index.html',
